8a4c15d20bcc6a08755855e6288589121f761253
sprokkel78 <sprokkel78.bart@gmail.com>
2025-09-18 13:18:24 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# THIS SCRIPT WILL INSTALL THE GGITHUB APP SYSTEM WIDE
+# THE SCRIPT MUST BE RUN WITH SUDO
+#
+# It will create a startup shell script named ggithub in /usr/bin,
+# the app will be placed in /usr/share/ggithub-sprokkel78
+# The .desktop file will be placed in /usr/share/applications/ as com.sprokkel78.ggithub.desktop
+
+mkdir -p /usr/share/ggithub-sprokkel78
+cp -r ./* /usr/share/ggithub-sprokkel78/
+echo "#!/bin/sh" > /usr/bin/ggithub
+echo "cd /usr/share/ggithub-sprokkel78" >> /usr/bin/ggithub
+echo "python3 ./ggithub.py" >> /usr/bin/ggithub
+cp ./ggithub.desktop /usr/share/applications/com.sprokkel78.ggithub.desktop
+chmod 755 /usr/bin/ggithub
+chmod 664 /usr/share/ggithub-sprokkel78/*
---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# THIS SCRIPT WILL REMOVE THE GGITHUB APP SYSTEM WIDE
+# THE SCRIPT MUST BE RUN WITH SUDO
+#
+# This will remove the ggithub app system wide.
+#
+
+rm -rf /usr/share/ggithub-sprokkel78
+rm /usr/bin/ggithub
+rm /usr/share/applications/com.sprokkel78.ggithub.desktop
